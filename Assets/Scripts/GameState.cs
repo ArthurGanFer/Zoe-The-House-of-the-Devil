5e5981ed551a2a4f448edae7e773e4f04c7d6878
caiosarb <Sb-caio@hotmail.com>
2025-02-04 11:21:11 -0500

    CHANGE TO HDRP
    Change the project from URP to HDRP

--- Assets/Scripts/GameState.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class GameState : MonoBehaviour
-{
-    public abstract GameState RunCurrentGameState();
-}
--- NOTE ON HOW BOSS WORKS.cs
+++
@@ -1,2 +0,0 @@
-//The Boss will Follow the Target but the Target needs to be animated to give it a path through the Room
-//The Player Must be on the Player Layer and Have the Player Tag
