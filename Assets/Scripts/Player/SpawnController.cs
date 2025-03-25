6ee6e9f072c7fef51eee956011605b2ba3b5eb95
AlltrixAtom <tristandantemota@gmail.com>
2025-03-25 12:29:25 -0400

    Fixed Checkpoint System

--- Assets/Scripts/Player/SpawnController.cs
+++ Assets/Scripts/Player/SpawnController.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnController : MonoBehaviour
 {
     private GameManager gameManager;
+    public Transform currentCheckpoint;
+ 
 
     private void Start()
     {
@@ -13,18 +14,64 @@ public class SpawnController : MonoBehaviour
         {
             Debug.Log("There is no GameObject of type GameManager in the scene!");
         }
+        else
+        {
+            ResetSpawn();
+        }
+    }
+
+    private void Update()
+    {
+        if (gameManager.playerSpawnPosition != currentCheckpoint.position || gameManager.playerSpawnRotation != currentCheckpoint.rotation)
+        {
+            gameManager.playerSpawnPosition = currentCheckpoint.position;
+            gameManager.playerSpawnRotation = currentCheckpoint.rotation;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (gameManager != null && other.gameObject.tag == "SpawnPoint")
+        if (gameManager != null && other.gameObject.tag == "CheckPoint")
         {
-            Vector3 spawnPoint = other.gameObject.transform.position;
-
-            if (spawnPoint != gameManager.playerSpawn)
-            {
-                gameManager.playerSpawn = spawnPoint;
-            }
+            SetCheckPoint(other.gameObject.transform);
         }
+        if (gameManager != null && other.gameObject.tag == "Goal")
+        {
+            gameManager.spawnSet = false;
+        }
+    }
+
+    private void ResetSpawn()
+    {
+        GameObject spawnObject = GameObject.FindGameObjectWithTag("SpawnPoint");
+        
+        if (spawnObject != null)
+        {
+            Transform spawnPoint = spawnObject.transform;
+
+            currentCheckpoint = spawnPoint;
+
+            if (!gameManager.spawnSet)
+            {
+                gameManager.playerSpawnPosition = currentCheckpoint.position;
+                gameManager.playerSpawnRotation = currentCheckpoint.rotation;
+                gameManager.spawnSet = true;
+            }
+            
+            gameManager.SetSpawn();
+
+            Debug.Log("Spawn has been set!");
+        }
+        else
+        {
+            Debug.Log($"There is no gameObject with the tag 'SpawnPoint' in {SceneManager.GetActiveScene().name}");
+        }
+    }
+
+    private void SetCheckPoint(Transform checkPoint)
+    {
+        currentCheckpoint = checkPoint;
+
+        Debug.Log("CheckPoint has been updated!");
     }
 }
